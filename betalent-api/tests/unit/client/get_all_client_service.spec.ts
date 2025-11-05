57a69d52fea1220e7406e92bdcd2f867ea09fe4f
Guilherme <guilherme1984.araujo@gmail.com>
2025-11-04 23:31:36 -0300

    feat:sparkless: Implementado o teste unitário getAll service

--- betalent-api/tests/unit/client/get_all_client_service.spec.ts
+++ betalent-api/tests/unit/client/get_all_client_service.spec.ts
@@ -1,23 +1,35 @@
 // tests/unit/client/get_all_client_service.spec.ts
 import { test } from '@japa/runner'
-import GetAllClientService from '#services/GetAllClientService'
 import ClientRepository from '../../../app/repositories/ClientRepository.js'
-
+import GetAllClientService from '#services/GetAllClientService'
+import Client from '#models/client'
 
 test.group('GetAllClientService', () => {
-  test('should return all clients', async ({ assert }) => {
+  test('deve retornar todos os clientes', async ({ assert }) => {
     const repo = new ClientRepository()
 
-    repo.getAll = async () => [
-      { id: 1, name: 'Guilherme', email: 'gui@example.com' },
-      { id: 2, name: 'Ana', email: 'ana@example.com' },
-    ]
+    // Mockando o método getAll
+    repo.getAll = async (): Promise<Client[]> => {
+      const client1 = new Client()
+      client1.id = 1
+      client1.name = 'Guilherme'
+      client1.email = 'gui@example.com'
+
+      const client2 = new Client()
+      client2.id = 2
+      client2.name = 'Ana'
+      client2.email = 'ana@example.com'
+
+      return [client1, client2]
+    }
 
     const service = new GetAllClientService(repo)
 
     const result = await service.execute()
 
+    assert.isArray(result)
     assert.lengthOf(result, 2)
     assert.equal(result[0].name, 'Guilherme')
+    assert.equal(result[1].email, 'ana@example.com')
   })
 })
